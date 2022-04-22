099d4fd8f92af39b59d6a96b9ff7406a97ea1e8c
kringelum94 <dbkringelum@gmail.com>
2022-04-22 23:43:06 +0200

    alpinejs added and index visually upgraded

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -1,3 +1,6 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy({
+        './node_modules/alpinejs/dist/cdn.js': './js/alpine.js',
+    });
 }
